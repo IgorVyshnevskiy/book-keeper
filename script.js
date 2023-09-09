3a8194c85ec70fd0337b22644b72f37ed75184ad
Igor Vyshnevskyi <igor.vyshnevskiy097@gmail.com>
2023-09-09 23:58:34 +0300

    added files and some code for practise

---
+++ script.js
@@ -0,0 +1,10 @@
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmark-form');
+const websiteName = document.getElementById('website-name');
+const websiteUrl = document.getElementById('website-url');
+const bookmarksContainer = document.getElementById('bookmarks-container');
+
+
+
